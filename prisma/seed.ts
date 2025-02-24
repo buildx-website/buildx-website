3972b7f047d57fb540587943331be0bf71dabf31
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-02-24 15:14:19 +0530

    seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,22 @@
-import prismaClient from '@/db';
+import { db } from '../src/db'
 import { Prisma } from '@prisma/client';
 
 const userData: Prisma.UserCreateInput[] = [{
-        email: 'test@test.com',
-        name: 'Test User',
-        password: 'password',
-    }, {
-        email: 'alice@gmail.com',
-        name: 'Alice',
-        password: 'password',
-    }
+    email: 'test@test.com',
+    name: 'Test User',
+    password: 'password',
+}, {
+    email: 'alice@gmail.com',
+    name: 'Alice',
+    password: 'password',
+}
 ]
 
 async function main() {
     try {
         await Promise.all(
             userData.map(async (user) => {
-                const insertUser = await prismaClient.user.upsert({
+                const insertUser = await db.user.upsert({
                     where: { email: user.email },
                     update: {},
                     create: user
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
 declare global {
+  // eslint-disable-next-line
   var prisma: PrismaClient | undefined;
 }
 
 export const prisma = globalThis.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
 
-export default prisma;
+export const db = prisma;
