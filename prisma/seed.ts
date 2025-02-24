b1f915c4bc20a03569541c1a0cbac72c0077cf49
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-02-24 14:44:51 +0530

    prisma init with seed

---
+++ prisma/seed.ts
@@ -0,0 +1,32 @@
+import prismaClient from '@/db';
+import { Prisma } from '@prisma/client';
+
+const userData: Prisma.UserCreateInput[] = [{
+        email: 'test@test.com',
+        name: 'Test User',
+        password: 'password',
+    }, {
+        email: 'alice@gmail.com',
+        name: 'Alice',
+        password: 'password',
+    }
+]
+
+async function main() {
+    try {
+        await Promise.all(
+            userData.map(async (user) => {
+                const insertUser = await prismaClient.user.upsert({
+                    where: { email: user.email },
+                    update: {},
+                    create: user
+                })
+                console.log("User created with id: ", insertUser.id)
+            })
+        )
+    } catch (error) {
+        console.error("Error creating user: ", error)
+    }
+}
+
+main();
