de67c9d9bf8e5ca5464befb0764aa72f30a2eee2
Sahil Tiwaskar <sahiltiwaskar.21@stvincentngp.edu.in>
2025-04-09 22:55:47 +0530

    ui update

--- src/lib/llm/chat.ts
+++ src/lib/llm/chat.ts
@@ -6,10 +6,9 @@ export async function chat(llm: OpenAI, prompt: string) {
     try {
         const completion = await llm.chat.completions.create({
             model: "qwen/qwen-2.5-coder-32b-instruct",
-            max_tokens: 200,
             messages: [{
                 role: "system",
-                content: "Return either node or react or nextjs based on what do you think this project should be. Only return a single word either 'node' or 'react' or 'nextjs'. Do not return anything extra."
+                content: "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra."
             }, {
                 role: "user",
                 content: prompt
